c1e38d12f7c8505fabe67dae156ee37c00b699ee
Hema123 <hemaghg@gmail.com>
2020-08-14 17:11:51 +0200

    testObject_Successfully_created()

--- src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
+++ src/test/java/se/lexicon/course_manager_assignment/model/StudentTest.java
@@ -24,8 +24,38 @@ public class StudentTest {
         assertEquals("India", testObject.getAddress());
 
     }
+    @Test
+
+    public void test_equals_and_hashcode() {
+        testObject = new Student(1, "hema", "hemaghg@gmail.com", "Vaxjo");
+        Student copy = new Student(1, "hema", "hemaghg@gmail.com", "Vaxjo");
+        assertEquals(copy, testObject);//equal method test
+        System.out.println("Test Object HashCode: " + testObject.hashCode());
+        System.out.println("Copy Object HashCode: " + copy.hashCode());
+        assertEquals(copy.hashCode(), testObject.hashCode());//hash code method test
+    }
+    @Test
+    public void test_toString_contains_correct_information() {
+
+        testObject = new Student(2, "Test", "hematest@gmail.com", "Test Address");
+
+        String toString = testObject.toString();
+
+        assertTrue(
+
+                toString.contains("2") &&
+
+                        toString.contains("Test") &&
+
+                        toString.contains("hematest@gmail.com") &&
+
+                        toString.contains("Test Address")
+
+        );
 
     }
+
+}
 
 
 
